2c00fc025cb6ec17f521b9d8beace044949ec9e9
rehman101 <73483363+rehman101@users.noreply.github.com>
2022-02-18 17:50:24 +0500

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
 use Stripe;
 
 class AdminController extends Controller
 {
     public function user()
     {
+
+
+
+
+
         $users=User::where('role','user')->get();
         return view('admin.user.index',compact('users'));
     }
