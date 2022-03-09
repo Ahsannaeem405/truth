abc4e8391da48f93889732ed9914789672c289f1
naeemliaqatweb <naeemliaqatweb@gmail.com>
2022-03-09 15:53:29 +0500

    dasd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -69,6 +69,74 @@
             border-radius: 5px;
             color:#fff;
         }
+
+        
+/* Shared */
+.loginBtn {
+  box-sizing: border-box;
+  position: relative;
+  /* width: 13em;  - apply for fixed size */
+  margin: 0.2em;
+  padding: 0 15px 0 46px;
+  border: none;
+  text-align: left;
+  line-height: 34px;
+  white-space: nowrap;
+  border-radius: 0.2em;
+  font-size: 16px;
+  color: #FFF;
+}
+.loginBtn:before {
+  content: "";
+  box-sizing: border-box;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 34px;
+  height: 100%;
+}
+.loginBtn:focus {
+  outline: none;
+}
+.loginBtn:active {
+  box-shadow: inset 0 0 0 32px rgba(0,0,0,0.1);
+}
+
+
+/* Facebook */
+.loginBtn--facebook {
+  background-color: #4C69BA;
+  background-image: linear-gradient(#4C69BA, #3B55A0);
+  cursor: pointer;
+
+  /*font-family: "Helvetica neue", Helvetica Neue, Helvetica, Arial, sans-serif;*/
+  text-shadow: 0 -1px 0 #354C8C;
+}
+.loginBtn--facebook:before {
+  border-right: #364e92 1px solid;
+  background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_facebook.png') 6px 6px no-repeat;
+}
+.loginBtn--facebook:hover,
+.loginBtn--facebook:focus {
+  background-color: #5B7BD5;
+  background-image: linear-gradient(#5B7BD5, #4864B1);
+}
+
+
+/* Google */
+.loginBtn--google {
+  /*font-family: "Roboto", Roboto, arial, sans-serif;*/
+  background: #b13525;
+  cursor: pointer;
+}
+.loginBtn--google:before {
+  border-right: #BB3F30 1px solid;
+  background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_google.png') 6px 6px no-repeat;
+}
+.loginBtn--google:hover,
+.loginBtn--google:focus {
+  background: #E74B37;
+}
     </style>
 
 </head>
@@ -106,17 +174,20 @@
                 </div>
                
             </form>
-            <a href="{{ url('auth/google') }}" class="w-100 py-2 my-3">
-                <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" >
-            </a>
+            <div class="row mt-3">
+                <div class="col-md-6">
+                    <a href="{{ url('auth/google') }}">  <button class="loginBtn loginBtn--facebook w-100">
+                Login with Facebook
+              </button></a>
+            </div>
+            <div class="col-md-6">
+                <a href="{{ url('auth/facebook') }}">  <button class="loginBtn loginBtn--google w-100">
+                Login with Google
+              </button></a>
+            </div>
+            </div>
+        
 
-            <a class="ml-1 btn btn-primary" href="{{ url('auth/facebook') }}" style="margin-top: 0px !important;background: blue;color: #ffffff;padding: 5px;border-radius:7px;" id="btn-fblogin">
-                <i class="fa fa-facebook-square" aria-hidden="true"></i> Login with Facebook
-            </a>
-           
-            <a href="{{ url('auth/google') }}"><button type="submit" class="googleBtn w-100 py-2 my-3"> <i class="ri-google-fill"></i> Goggle</button></a>
-
-           <a href=""> <button type="submit" class="facebookBtn w-100 py-2 my-3"> <i class="ri-facebook-fill"></i> Facebook</button></a>
         </div>
     </div>
 </main>
